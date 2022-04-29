34ff49cfbaf32843849beeb3826c9d70204466e1
yusoffnike01 <ronaldoyusoff@gmail.com>
2022-04-29 11:43:11 +0800

    remove github page

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
   return (
     <div className="App">
      <h1>Magic Match</h1>
-     <button onClick={shuffleCards}>New Games</button>
+     <button onClick={shuffleCards}>New Game</button>
      <div className="card-grid">
        {
          cards.map(card=>(
